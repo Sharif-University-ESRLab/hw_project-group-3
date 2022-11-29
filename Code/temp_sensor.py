5b9b6f9b08a2fc23d8cb203f5f44be9bf0285231
bahar <baharkh127@gmail.com>
2022-11-29 20:46:00 +0330

    fixed the bug for temp sensor

--- Code/temp_sensor.py
+++ Code/temp_sensor.py
@@ -6,7 +6,6 @@ os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
  
 base_dir = '/sys/bus/w1/devices/'
-#device_folder = glob.glob(base_dir + '28*')[0]
 device_folders = glob.glob(base_dir + '28*')
 
  
@@ -20,7 +19,7 @@ def read_temp_raw(device_folder):
  
 def read_temp():
 
-    for df in device_folders:
+    for i, df in enumerate(device_folders):
         lines = read_temp_raw(df)
         while lines[0].strip()[-3:] != 'YES':
             time.sleep(0.2)
@@ -30,8 +29,8 @@ def read_temp():
             temp_string = lines[1][equals_pos+2:]
             temp_c = float(temp_string) / 1000.0
             temp_f = temp_c * 9.0 / 5.0 + 32.0
-            return temp_c, temp_f
+            print("Sensor {} => {} (C) - {} (F)".format(i + 1, temp_c, temp_f))
 	
 while True:
-	print(read_temp())	
-	time.sleep(1)
+    read_temp()
+    time.sleep(1)
