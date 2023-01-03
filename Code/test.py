e1956d51a23363c61142fd64b5ea42a7257301fb
bahar <baharkh127@gmail.com>
2023-01-03 23:40:47 +0330

    added 4 new directions

--- Code/test.py
+++ Code/test.py
@@ -1,5 +1,6 @@
 
 # function for generating manual coordinates
+# direction: one of 8 directions => N,S,E,W,NE,NW,SE,SW
 def get_coordinates_manual(start_lat=35.703129, start_long=51.351671, direction='N', step=0.001):
     curr_latitude = start_lat
     curr_longitude = start_long
@@ -10,7 +11,19 @@ def get_coordinates_manual(start_lat=35.703129, start_long=51.351671, direction=
         curr_latitude -= step
     elif direction == 'E':
         curr_longitude += step
+    elif direction == 'W':
+        curr_longitude -= step
+    elif direction == "NE":
+        curr_latitude += step
+        curr_longitude += step
+    elif direction == "NW":
+        curr_latitude += step
+        curr_longitude -= step
+    elif direction == "SE":
+        curr_latitude -= step
+        curr_longitude += step
     else:
+        curr_latitude -= step
         curr_longitude -= step
 
     return start_lat, start_long, curr_latitude, curr_longitude
