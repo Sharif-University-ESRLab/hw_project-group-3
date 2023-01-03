2c116b3907d72ce25fd8e355e396970e09b1b71e
bahar <baharkh127@gmail.com>
2023-01-03 15:13:24 +0330

    added code for turning off and on the LEDs

---
+++ Code/led.py
@@ -0,0 +1,51 @@
+import RPi.GPIO as GPIO
+import time
+
+
+led_id_pin = {
+    1: 5,
+    2: 6,
+    3: 13,
+    4: 19,
+}
+
+def turn_on_led(led_id):
+    GPIO.output(led_id_pin[led_id], GPIO.HIGH)
+    print("LED {} on".format(led_id))
+
+
+def turn_off_led(led_id):
+    GPIO.output(led_id_pin[led_id], GPIO.LOW)
+    print("LED {} off".format(led_id))
+
+def prep_leds():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+
+    for pin in led_id_pin.values():
+        print(pin)
+        GPIO.setup(pin, GPIO.OUT)
+
+def test_leds():
+    while True():
+        turn_off_led(4)
+        turn_on_led(1)
+        time.sleep(1)
+
+        turn_off_led(1)
+        turn_on_led(2)
+        time.sleep(1)
+
+        turn_off_led(2)
+        turn_on_led(3)
+        time.sleep(1)
+        
+        turn_off_led(3)
+        turn_on_led(4)
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    prep_leds()
+    test_leds()
+
